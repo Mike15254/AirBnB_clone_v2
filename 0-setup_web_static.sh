bd2dda3647c703654fec99cab48456dc0baee237
Mike15254 <mike8max10@gmail.com>
2022-10-15 19:05:45 +0000

    update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,43 +1,13 @@
 #!/usr/bin/env bash
-# sets up server
+# Script that configures Nginx server with some folders and files
 apt-get -y update
 apt-get -y install nginx
-mkdir -p /data/web_static/shared/ /data/web_static/releases/test/
-echo \
-'<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>' > /data/web_static/releases/test/index.html
-rm -f /data/web_static/current
+service nginx start
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-chown -hR ubuntu:ubuntu /data
-CONF="server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
-	root /var/www/html;
-	index index.html index.htm index.nginx-debian.html;
-	server_name _;
-	location / {
-		try_files \$uri \$uri/ =404;
-	}
-	if (\$request_filename ~ redirect_me){
-		rewrite ^ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;
-	}
-	error_page 404 /404.html;
-	location = /404.html {
-		root /var/www/error/;
-		internal;
-	}
-	location /hbnb_static/ {
-		alias /data/web_static/current/;
-	}
-<<<<<<< HEAD
-=======
->>>>>>> 1dcc7d46e29430daba96ebe9887fe39079e91e36
-    add_header X-Served-By $HOSTNAME;
-}"
-echo "$CONF" > /etc/nginx/sites-available/default
-service nginx reload
+chown -R ubuntu:ubuntu /data/
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
+service nginx restart
+exit 0
